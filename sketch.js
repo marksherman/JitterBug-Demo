1f40747c2511a4e07ec11f70c1e46530accb61d2
Mark Sherman <shermanm@emmanuel.edu>
2020-05-04 12:05:06 -0400

    Constructor function

--- sketch.js
+++ sketch.js
@@ -1,36 +1,28 @@
-var bug = {
-  x: 0,
-  y: 0,
-  move: function() {
-    this.x += random(-5, 5);
-    this.y += random(-5, 5);
-    fill("orange");
-    ellipse(this.x, this.y, 5);
+function makeBug(speed, color) {
+  var bug = {
+    x: width / 2,
+    y: height / 2,
+    move: function() {
+      this.x += random(-speed, speed);
+      this.y += random(-speed, speed);
+      fill(color);
+      ellipse(this.x, this.y, 5);
+    }
   }
+
+  return bug;
 }
 
-var bug2 = {
-  x: 0,
-  y: 0,
-  move: function() {
-    this.x += random(-1, 1);
-    this.y += random(-1, 1);
-    fill("purple");
-    ellipse(this.x, this.y, 5);
-  }
-}
+var bug;
 
 function setup() {
   createCanvas(400, 400);
   background(220);
   noStroke();
-  bug.x = height / 2;
-  bug.y = width / 2;
-  bug2.x = height / 2;
-  bug2.y = width / 2;
+
+  bug = makeBug(2, "purple");
 }
 
 function draw() {
   bug.move();
-  bug2.move();
 }
