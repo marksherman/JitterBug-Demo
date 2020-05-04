5b281656b25524e36381f51185e946d3b755c74e
Mark Sherman <shermanm@emmanuel.edu>
2020-05-04 12:04:28 -0400

    Bug Objects with Functions

--- sketch.js
+++ sketch.js
@@ -1,7 +1,36 @@
+var bug = {
+  x: 0,
+  y: 0,
+  move: function() {
+    this.x += random(-5, 5);
+    this.y += random(-5, 5);
+    fill("orange");
+    ellipse(this.x, this.y, 5);
+  }
+}
+
+var bug2 = {
+  x: 0,
+  y: 0,
+  move: function() {
+    this.x += random(-1, 1);
+    this.y += random(-1, 1);
+    fill("purple");
+    ellipse(this.x, this.y, 5);
+  }
+}
+
 function setup() {
   createCanvas(400, 400);
+  background(220);
+  noStroke();
+  bug.x = height / 2;
+  bug.y = width / 2;
+  bug2.x = height / 2;
+  bug2.y = width / 2;
 }
 
 function draw() {
-  background(220);
+  bug.move();
+  bug2.move();
 }
